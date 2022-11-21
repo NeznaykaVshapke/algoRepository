481dda6342af46a4db37cab69de85e864cd37cf3
NeznaykaVshapke <112863682+NeznaykaVshapke@users.noreply.github.com>
2022-11-21 08:31:15 +0300

    Update Fcont2.cpp

--- Fcont2.cpp
+++ Fcont2.cpp
@@ -1,35 +1,33 @@
 #include <iostream>
 #include <vector>
-using X = std::vector<uint64_t>;
-using U = uint64_t;
-const U kArr1 = (static_cast<U>(1) << 9) - 1;
-inline void ZifrSort(X& massiv, int razr) {
-  const U kArr2 = static_cast<U>(1) << ((razr - 1) * 8);
-  int size_massiv = massiv.size();
-  X cnt(kArr1 + 1, 0);
-  X massiv2(size_massiv, 0);
-  U t;
-  U delta = (kArr1 << (razr - 1) * 8);
-  for (int i = 0; i < size_massiv; ++i) {
+const uint64_t kArr1 = (static_cast<uint64_t>(1) << 9) - 1;
+inline void LSDSort(std::vector<uint64_t>& massiv, int razr) {
+  const uint64_t kArr2 = static_cast<uint64_t>(1) << ((razr - 1) * 8);
+  size_t size_massiv = massiv.size();
+  std::vector<uint64_t> cnt(kArr1 + 1, 0);
+  std::vector<uint64_t> massiv2(size_massiv, 0);
+  uint64_t t;
+  uint64_t delta = (kArr1 << (razr - 1) * 8);
+  for (size_t i = 0; i < size_massiv; ++i) {
     t = massiv[i] & delta;
     t /= kArr2;
     cnt[t]++;
     massiv2[i] = massiv[i];
   }
-  X summ(kArr1 + 1, 0);
+  std::vector<uint64_t> summ(kArr1 + 1, 0);
   for (size_t i = 1; i < summ.size(); ++i) {
     summ[i] = cnt[i - 1] + summ[i - 1];
   }
-  for (int i = 0; i < size_massiv; ++i) {
+  for (size_t i = 0; i < size_massiv; ++i) {
     t = massiv2[i] & delta;
     t /= kArr2;
     massiv[summ[t]++] = massiv2[i];
   }
 }
 
-inline void LSD(X& massiv) {
-  for (int i = 1; i <= 8; ++i) {
-    ZifrSort(massiv, i);
+inline void LSD(std::vector<uint64_t>& massiv) {
+  for (size_t i = 1; i <= 8; ++i) {
+    LSDSort(massiv, i);
   }
 }
 
@@ -37,10 +35,10 @@ int main() {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(NULL);
   std::cout.tie(NULL);
-  int n;
+  size_t n;
   std::cin >> n;
-  X massiv(n);
-  for (int i = 0; i < n; i++) {
+  std::vector<uint64_t> massiv(n);
+  for (size_t i = 0; i < n; i++) {
     std::cin >> massiv[i];
   }
   LSD(massiv);
