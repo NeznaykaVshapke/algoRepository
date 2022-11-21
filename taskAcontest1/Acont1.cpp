7b2bcd3a882da1d8873820091a79354ded7086e2
NeznaykaVshapke <112863682+NeznaykaVshapke@users.noreply.github.com>
2022-11-21 08:08:48 +0300

    Update Acont1.cpp

--- taskAcontest1/Acont1.cpp
+++ taskAcontest1/Acont1.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-bool BinarySearch(const size_t* begin, const size_t* end, size_t target) {
+bool BinarySearch(const size_t* begin, const size_t* end, int target) {
   size_t left = 0;
   size_t right = end - begin;
   while (right >= left + 2) {
