d3bb320c773c339a15752e81fae99502dd3399d2
NeznaykaVshapke <112863682+NeznaykaVshapke@users.noreply.github.com>
2022-11-21 08:06:19 +0300

    Update Acont1.cpp

--- taskAcontest1/Acont1.cpp
+++ taskAcontest1/Acont1.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-bool BinarySearch(const int* begin, const int* end, int target) {
-  int left = 0;
-  int right = end - begin;
+bool BinarySearch(const size_t* begin, const size_t* end, size_t target) {
+  size_t left = 0;
+  size_t right = end - begin;
   while (right >= left + 2) {
-    int centre = (left + right) / 2;
+    size_t centre = (left + right) / 2;
     if (*(centre + begin) > target) {
       right = centre;
     } else {
@@ -13,18 +13,18 @@ bool BinarySearch(const int* begin, const int* end, int target) {
   return (target == *(left + begin));
 }
 int main() {
-  int long_arr;
+  size_t long_arr;
   std::cin >> long_arr;
   int* arr = new int[long_arr];
-  for (int i = 0; i < long_arr; ++i) {
+  for (size_t i = 0; i < long_arr; ++i) {
     std::cin >> arr[i];
   }
-  int q;
+  size_t q;
   std::cin >> q;
-  int begin;
-  int end;
+  size_t begin;
+  size_t end;
   int target;
-  for (int i = 0; i < q; ++i) {
+  for (size_t i = 0; i < q; ++i) {
     std::cin >> begin >> end >> target;
     int* old_arr = arr;
     bool ans = BinarySearch(old_arr + begin, old_arr + end, target);
