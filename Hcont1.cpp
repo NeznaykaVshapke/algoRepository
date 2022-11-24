659361d30b5353a8ba254a5055f65e684772a988
NeznaykaVshapke <112863682+NeznaykaVshapke@users.noreply.github.com>
2022-11-24 10:03:50 +0300

    Update Hcont1.cpp

--- Hcont1.cpp
+++ Hcont1.cpp
@@ -1,20 +1,17 @@
 #include <cmath>
 #include <iomanip>
 #include <iostream>
-inline void SrGeom(double* arr, int& longarr) {
-  std::ios_base::sync_with_stdio(false);
-  std::cin.tie(NULL);
-  std::cout.tie(NULL);
-  for (int i = 1; i < longarr; i++) {
+inline void SrGeom(double* arr, size_t& longarr) {
+  for (size_t i = 1; i < longarr; i++) {
     double srgeom = pow(arr[i], (1.0 / (i + 1.0)));
     double srgeom1 = pow(arr[i - 1], (i * 1.0) / (i + 1.0));
     arr[i] = srgeom * srgeom1;
   }
-  int q;
+  size_t q;
   std::cin >> q;
-  for (int i = 0; i < q; i++) {
-    int begin;
-    int end;
+  for (size_t i = 0; i < q; i++) {
+    size_t begin;
+    size_t end;
     std::cin >> begin;
     std::cin >> end;
     begin--;
@@ -37,10 +34,10 @@ int main() {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(NULL);
   std::cout.tie(NULL);
-  int longarr;
+  size_t longarr;
   std::cin >> longarr;
   double* arr = new double[longarr];
-  for (int i = 0; i < longarr; i++) {
+  for (size_t i = 0; i < longarr; i++) {
     std::cin >> arr[i];
   }
   SrGeom(arr, longarr);
